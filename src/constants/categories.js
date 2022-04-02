192d4c883c8481ef440799cef22c41f85f97abed
nachop96 <ignapedrosa@gmail.com>
2022-04-02 15:30:59 -0300

    Prueba

---
+++ src/constants/categories.js
@@ -0,0 +1,31 @@
+export const CATEGORIES = [
+
+    { 
+        id: 1,
+        name: 'Harinas',
+        color: '#FFC107',
+        description, 'Descripition 1',
+    },
+
+    {
+        id: 2,
+        name: 'Lacteos',
+        color: '#00BD9D',
+        description, 'Descripition 2',
+    },
+
+    {
+        id: 3,
+        name: 'Frutas',
+        color: '#FF5722',
+        description, 'Descripition 3',
+    },
+
+    {
+        id: 4,
+        name: 'Verduras',
+        color: '#00BFA5',
+        description, 'Descripition 4',
+    },
+
+    export default CATEGORIES;
--- src/navigation/main.js
+++ src/navigation/main.js
@@ -1,18 +1,33 @@
 import React from 'react';
+import {Platform} from 'react-native';
 
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/home";
 import Category from "../screens/category";
 import Product from "../screens/product";
-
+import { colors } from '../constants/themes';
 
 const Stack = createNativeStackNavigator();
 
 const MainNavigator = () => {
 
     return (
-        <Stack.Navigator initialRouteName="Home" >
-            <Stack.Screen name="Home" component={Home} options={{ title: 'Inicio' }}/>
+        <Stack.Navigator 
+        initialRouteName="Home"
+        screenOptions={{ 
+            headerStyle:{
+                backgroundColor: Platform.OS === 'android' ? colors.primary : ''
+
+            },
+            headerTintColor: Platform.OS === 'android' ? 'white' : colors.primary,
+            headerTitleStyle:{
+                fontFamily: 'OpenSans',
+
+            },
+            backgroundColor: colors.secondary,
+            
+        }} >
+            <Stack.Screen name="Home" component={Home} options={{ headerShown: false, }}/>
             <Stack.Screen name="Product" component={Product} options={{ title: 'Productos' }} />
             <Stack.Screen name="Category" component={Category} options={{ title: 'Categorias' }}/>
         </Stack.Navigator>
